06b47b5faad91e9b9882d87b5af56ccf49b1ddf7
aditya <adityaachaudhary2003@gmail.com>
2023-10-08 21:48:44 +0530

    Extracting and saving scraped data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+from csv import writer
+import os
 
 url = "https://www.worldometers.info/world-population/"
 
@@ -12,3 +14,21 @@ def scrape(url):
 
 
 scraped = scrape(url)
+
+tabl = scraped.find('table', id="popbycountry")
+
+header = []  # Created empty list
+# Extract Header Row
+for i in tabl.find_all('th'):
+    header.append(i.text)
+
+path = os.path.join(os.getcwd(), "world_population_by_country.csv")
+
+with open(path, 'wt', newline='', encoding='utf-8') as csv_file:
+    csv_writer = writer(csv_file, delimiter='|')
+    csv_writer.writerow(header)  # write header
+    # Write data to csv file
+    for row in tabl.find_all('tr')[1:]:
+        td = row.find_all('td')
+        r = [i.text.replace('\n', '') for i in td]
+        csv_writer.writerow(r)
